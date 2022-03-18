473ee577be6a5079e45c78b47c208f39272b2715
Marcelus Kyalo <kmarcelus92@gmail.com>
2022-03-18 08:19:00 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,16 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type Services struct {
+	Service_id          int
+	Service_name        string
+	Service_price       string
+	Service_duration    string
+	Service_category    string
+	Service_description string
+	Created_on          string
+}
+
 func main() {
 	fmt.Println("Go SQl")
 
@@ -19,5 +29,28 @@ func main() {
 
 	defer db.Close()
 
-	fmt.Println("succesfully connected to sql")
+	// insert, err := db.Query("INSERT INTO tester VALUES('ELLIOT')")
+
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// defer insert.Close()
+	//fmt.Printf("Inserted")
+
+	results, err := db.Query("SELECT * FROM services")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	for results.Next() {
+		var services Services
+
+		err = results.Scan(&services.Service_id, &services.Service_name, &services.Service_price, &services.Service_duration, &services.Service_category, &services.Service_description, &services.Created_on)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Println(services)
+	}
 }
